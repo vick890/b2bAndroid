791498f368f29b0878d90bde1c3db80345a95957
Rahul <rahul.dhar89@gmail.com>
2020-07-01 16:29:04 +0530

    Update Constants.java

--- app/src/main/java/com/golink/ecommerceb2b/Utils/Constants.java
+++ app/src/main/java/com/golink/ecommerceb2b/Utils/Constants.java
@@ -3,49 +3,5 @@ package com.golink.ecommerceb2b.Utils;
 public class Constants {
 
 
-   // private static final String COMMON_URL = "https://www.cartprod.ml/ecommerce/api/";
-    private static final String COMMON_URL = "https://www.brolabs.xyz/ecommerce/api/";
-    //public static final String IMAGE_URL = "https://www.cartprod.ml/ecommerce/";
-    public static final String IMAGE_URL = "https://www.brolabs.xyz/ecommerce/";
-
-
-    public static final String SIGNUP = COMMON_URL + "userLoginSign";
-//    public static final String LOGIN = COMMON_URL + "user_login";
-    public static final String OTP = COMMON_URL + "validateOtp";
-    public static final String RESEND_OTP = COMMON_URL + "resendOtp";
-//    public static final String UPDATE_ADDRESS = COMMON_URL + "saveAddress";
-    public static final String UPDATE_PROFILE = COMMON_URL + "saveProfile";
-    public static final String SAVE_IMAGE = COMMON_URL + "addUserImage";
-    public static final String ALL_VENDORS = COMMON_URL + "vendors";
-    public static final String ALL_PRODUCTS_BY_VENDOR = COMMON_URL + "productByVendorId";
-    public static final String ALL_PRODUCTS = COMMON_URL + "product";
-    public static final String ALL_PRODUCTS_BY_ALL_USER_VENDOR = COMMON_URL + "productByAllUsersVendor";
-    public static final String VENDOR_DETAIL = COMMON_URL + "vendor_detail";
-    public static final String PRODUCT_DETAIl = COMMON_URL + "product_detail";
-    public static final String PRODUCT_IMAGES = COMMON_URL + "productImages";
-    public static final String ADD_CART = COMMON_URL + "addCart";
-    public static final String CART = COMMON_URL + "cart";
-    public static final String DELETE_CART = COMMON_URL + "deleteCart";
-    public static final String ADD_WISHLIST = COMMON_URL + "addWishlist";
-    public static final String REQUEST_CHECK = COMMON_URL + "userRequests";
-    public static final String SEND_REQUEST = COMMON_URL + "saveRequest";
-    public static final String PAYMENT = COMMON_URL + "order_payment";
-    public static final String ORDERS = COMMON_URL + "orders";
-    public static final String ORDER_PRODUCTS = COMMON_URL + "orderProducts";
-    public static final String CHECK_REQUEST = COMMON_URL + "checkRequest";
-    public static final String CANCEL_ORDER = COMMON_URL + "cancelOrder";
-    public static final String USER_PROFILE = COMMON_URL + "userProfile";
-//    public static final String WALLET = COMMON_URL + "userWalletAmount";
-//    public static final String SUBSCRIPTION = COMMON_URL + "userSubscriptionProduct";
-//    public static final String DELIVER_ADDRESS = COMMON_URL + "deliverAddress";
-//    public static final String DELIVER_TIME = COMMON_URL + "timeSlot";
-//    public static final String OFFER_CODE = COMMON_URL + "offerCode";
-//    public static final String RBPA = COMMON_URL + "refferBonusPaybleAmount";
-//    public static final String ADD_IMAGE = COMMON_URL + "addUserImage";
-//    public static final String CHECK_FIRST = COMMON_URL + "checkFirstOrder";
-//    public static final String HASH = COMMON_URL + "hash";
-//    public static final String ADMIN_NOT = COMMON_URL + "admin_notification";
-//    public static final String BLOCK_CHECK = COMMON_URL + "checkDeleteUser";
-    // data = 1 - first
-
+  
 }
